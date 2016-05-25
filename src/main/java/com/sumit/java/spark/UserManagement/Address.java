a90170cb4a04a30e20887dc4c278f9802efa2f93
sumitsikhwal <sumitksikhwal@gmail.com>
2016-05-25 11:44:58 -0500

    Update Address.java

--- src/main/java/com/sumit/java/spark/UserManagement/Address.java
+++ src/main/java/com/sumit/java/spark/UserManagement/Address.java
@@ -1,49 +1,46 @@
 package com.sumit.java.spark.UserManagement;
 
-
-	public class Address {
+public class Address {
+	private String street;
+	private String city;
+	private String zipcode;
+	private String state;
+	private String country;
 	     
-	    private String street;
-	    private String city;
-	    private String zipcode;
-	    private String state;
-	    private String country;
-	    
-	     
-	    public String getStreet() {
-	        return street;
-	    }
-	    public void setStreet(String street) {
-	        this.street = street;
-	    }
-	    public String getState() {
-		return state;
-	    }
-	    public void setState(String state) {
-		this.state = state;
-	    }
-	    public String getCountry() {
-		return country;
-	    }
-	    public void setCountry(String country) {
-		this.country = country;
-	    }
-	    public String getCity() {
-	        return city;
-	    }
-	    public void setCity(String city) {
-	        this.city = city;
-	    }
-	    public String getZipcode() {
-	        return zipcode;
-	    }
-	    public void setZipcode(String zip) {
-	        this.zipcode = zip;
-	    }
-	     
-	    @Override
-	    public String toString(){
-	        return getStreet() + ", "+getCity()+", "+getZipcode();
-	    }
+	public String getStreet() {
+	     return street;
 	}
+	public void setStreet(String street) {
+	     this.street = street;
+	}
+	public String getState() {
+	     return state;
+	}
+	public void setState(String state) {
+	     this.state = state;
+        }
+	public String getCountry() {
+	     return country;
+	}
+	public void setCountry(String country) {
+	     this.country = country;
+	}
+	public String getCity() {
+	     return city;
+	}
+	public void setCity(String city) {
+	     this.city = city;
+	}
+	public String getZipcode() {
+	     return zipcode;
+	}
+	public void setZipcode(String zip) {
+	     this.zipcode = zip;
+	}
+	     
+	@Override
+	public String toString(){
+	    return getStreet() + ", "+getCity()+", "+getZipcode();
+	}
+}
 

f74027d4bacb28e7795ff5c137c4e294cb6f13e2
sumitsikhwal <sumitksikhwal@gmail.com>
2016-05-25 11:03:03 -0500

    Update Company.java

--- src/main/java/com/sumit/java/spark/UserManagement/Company.java
+++ src/main/java/com/sumit/java/spark/UserManagement/Company.java
@@ -1,24 +1,24 @@
 package com.sumit.java.spark.UserManagement;
 
 public class Company {
-	private String name;
-	private String website;
+     private String name;
+     private String website;
 
 public Company(){}
 
 public String getName() {
-	return name;
+     return name;
 }
 
 public void setName(String name) {
-	this.name = name;
+     this.name = name;
 }
 
 public String getWebsite() {
-	return website;
+     return website;
 }
 
 public void setWebsite(String website) {
-	this.website = website;
+     this.website = website;
 }
 }

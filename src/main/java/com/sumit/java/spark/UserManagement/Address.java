eff8658ba5083208a86d037a28ce5ec56cfc62e8
sumitsikhwal <sumitksikhwal@gmail.com>
2016-05-25 11:00:38 -0500

    Update Address.java

--- src/main/java/com/sumit/java/spark/UserManagement/Address.java
+++ src/main/java/com/sumit/java/spark/UserManagement/Address.java
@@ -17,18 +17,18 @@ package com.sumit.java.spark.UserManagement;
 	        this.street = street;
 	    }
 	    public String getState() {
-			return state;
-		}
-		public void setState(String state) {
-			this.state = state;
-		}
-		public String getCountry() {
-			return country;
-		}
-		public void setCountry(String country) {
-			this.country = country;
-		}
-		public String getCity() {
+		return state;
+	    }
+	    public void setState(String state) {
+		this.state = state;
+	    }
+	    public String getCountry() {
+		return country;
+	    }
+	    public void setCountry(String country) {
+		this.country = country;
+	    }
+	    public String getCity() {
 	        return city;
 	    }
 	    public void setCity(String city) {
